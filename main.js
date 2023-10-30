68f82ebedfff698bfb85a3840067c23ed1cdae72
marianaitaly <127768432+marianaitaly@users.noreply.github.com>
2023-10-30 12:27:54 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
 function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+    const elemento = document.querySelector(idElementoAudio).play();
+
+    if(elemento === null){
+        alert('Elemento não encontrado');
+    }
 }
 const listaDeTeclas=document.querySelectoraAll('.tecla');
-
-
 //Estrutura de repetição - Enquanto
-for(contador = 0; contado < listaDeTeclas.length; contador++){
+for( let contador = 0; contado < listaDeTeclas.length; contador++){
 const tecla = listaDeTeclas[contador];
 const instrumento = tecla.classList[1];
 const idAudio = `#som_ ${instrumento}`;
+
 console.log(idAudio);
 
 tecla.onclick = function () {
     tocaSom(idAudio);
 };
-tecla.onkeydown = function(){
+tecla.onkeydown = function(evento){
+    if(evento.code === 'Enter'|| evento.code === 'Space'){
     tecla.classList.add('ativa');
+    }
 };
 tecla.onkeyup = function(){
     tecla.classList.remove('ativa');
 };
+    console.log(instrumentos);
     console.log(contador);
 }
